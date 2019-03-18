c86d163180de1a2272b6a2c49f4010654e2d054f
Ingrubenl <ruben.lasso88@hotmail.com>
2019-03-17 23:33:19 -0500

    operaciones aritmeticas php

---
+++ operaciones.php
@@ -0,0 +1,12 @@
+<?php
+    $num1=15;
+    $num2= 23;
+   
+
+    echo "La suma de ".$num1."+".$num2." es de ".$num1+$num2;
+    echo "La resta de ".$num1."+".$num2." es de ".$num1-$num2;
+    echo "La multiplicacion de ".$num1."+".$num2." es de ".$num1*$num2;
+    echo "La division de ".$num1."+".$num2." es de ".$num1/$num2;
+
+
+?>
